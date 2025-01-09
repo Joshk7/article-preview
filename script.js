7573cc65bbdaa8d29606950b0f2bf91d01e78a8b
JoshK7 <joshk7683@gmail.com>
2025-01-08 22:04:37 -0600

    added logic to make popups work

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
 const shareButton = document.getElementById("share-button");
+const shareIcon = document.getElementById("share-icon");
+const popup = document.getElementById("popup");
+const upshift = document.getElementById("upshift");
 
+const mediaQuery = window.matchMedia("(min-width: 57.5rem)");
 
-shareButton.addEventListener("onclick", () => {
+shareButton.addEventListener("click", () => {
+    shareButton.classList.toggle("active__button");
+    shareIcon.classList.toggle("active__icon");
 
+    if (mediaQuery.matches) {
+        popup.classList.toggle("visibility");
+    } else {
+        upshift.classList.toggle("visibility");
+    }
+});
+
+window.addEventListener("resize", () => {
+    shareButton.classList.remove("active__button");
+    shareIcon.classList.remove("active__icon");
+    popup.classList.add("visibility");
+    upshift.classList.add("visibility");
 });
